9df4c128b2a853344ddd0ee7946f7870b28b8c7a
devansh67 <devansh67@gmail.com>
2025-11-29 18:41:52 +0530

    Example implementations of Lambda and Stream methods

--- MainClass.java
+++ MainClass.java
@@ -11,7 +11,49 @@ import java.util.stream.Stream;
 
 public class MainClass {
     public static void main(String[] args) {
+//        Walkable obj = (steps, isEnabled) -> {
+//            System.out.println("Walking fast "+ steps + " steps.");
+//            return 2*steps;
+//        };
+//
+//        int answer = obj.walk(4, true);
+//        System.out.println("Twice the number of steps is: "+ answer);
 
+        List<String> fruits = List.of("Banana", "Apple", "Kiwi", "Mango");
+        Stream<String> stream = fruits.stream();
+
+//        stream.forEach((fruit) -> {
+//            System.out.println(fruit);
+//        });
+        // stream once used is closed and cannot be used again, so the same thing above cannot be done again
+        // this is used for chain pattern where stream is created everytime for different kind of operations to be carried on it
+        // this is to prevent memory leaks as stream gets destroyed once its been used
+
+//        Set<Integer> fruitsList = fruits
+//                .stream()
+//                .map(fruit -> fruit.length())
+//                .collect(Collectors.toSet());
+//
+//        System.out.println(fruitsList);
+
+        Map<String, Integer> fruitsList = fruits
+                .stream()
+                .collect(Collectors.toMap(
+                        fruit -> fruit,
+                        String::length
+                ));
+
+        System.out.println(fruitsList);
+
+//        stream
+//                .filter(fruit -> fruit.length() < 5)
+//                .sorted()
+//                .map(fruit -> fruit.length())// this changed the flow as fruit became an int here helping us avoid multiple variable and function declarations
+//                .map(fruitLength -> 2*fruitLength)
+//                .forEach(System.out::println);
     }
 
+//    interface Walkable {
+//        int walk(int steps, boolean isEnabled);
+//    }
 }
