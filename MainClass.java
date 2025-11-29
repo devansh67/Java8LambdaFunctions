4c9272892cfb80a64654e6c306280f43e359fd88
devansh67 <devansh67@gmail.com>
2025-11-29 18:21:25 +0530

    Files

---
+++ MainClass.java
@@ -0,0 +1,17 @@
+package streamAPI;
+
+//Lambda, Lambda method inference,
+// Stream, sorting, filter, map, Collectors
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+    }
+
+}
